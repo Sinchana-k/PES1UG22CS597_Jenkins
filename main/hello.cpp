fc0fe4a96d678155170a2c6f97d4cccca2ab748b
Sinchana-k <sinchanakc51103@gmail.com>
2025-03-13 15:24:26 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main(void) {
+  cout << "PES1UG22CS597 Sinchana K C\n";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
